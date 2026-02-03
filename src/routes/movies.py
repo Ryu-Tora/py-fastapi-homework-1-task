6f360a78d2566944fe5d027857df6988d453b1d9
Adam Halka <galkaadam4@gmail.com>
2026-02-03 15:44:20 +0100

    Solution

--- src/routes/movies.py
+++ src/routes/movies.py
@@ -1,10 +1,30 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
 
 from database import get_db, MovieModel
-
+from schemas import MovieListResponseSchema, MovieDetailResponseSchema
 
 router = APIRouter()
 
-# Write your code here
+
+@router.get("/movies/", response_model=MovieListResponseSchema)
+async def get_movies(db: AsyncSession = Depends(get_db), page: int = Query(1, ge=1), per_page: int = Query(10, ge=1, le=20)):
+    result = await db.execute(select(MovieModel).limit(per_page).offset((page - 1) * per_page))
+    movies = result.scalars().all()
+
+    if not movies:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "No movies found.")
+
+    return movies
+
+
+@router.get("/movies/{movie_id}", response_model=MovieDetailResponseSchema)
+async def get_movie(db: AsyncSession = Depends(get_db), movie_id: int | None = None):
+    result = await db.exeute(select(MovieModel).where(MovieModel.id == movie_id))
+    movie = result.scalars().first()
+
+    if not movie:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Movie with the given ID was not found")
+    return movie
--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,39 @@
-# Write your code here
+from datetime import date
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+
+
+class MovieBase(BaseModel):
+    name: str
+    date: date
+    score: float
+    genre: str
+    overview: str
+    crew: list
+    orig_title: str
+    status: str
+    orig_lang: str
+    budget: float
+    revenue: float
+    country: str
+
+
+class MovieCreate(MovieBase):
+    pass
+
+
+class MovieDetailResponseSchema(MovieBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+
+class MovieListResponseSchema(BaseModel):
+    movies: List[MovieDetailResponseSchema]
+    prev_page: Optional[int]
+    next_page: Optional[int]
+    total_pages: int
+    total_items: int
